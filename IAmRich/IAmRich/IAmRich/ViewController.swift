7f829207a15fbb23c7e518dcf3a037353773c36b
Kauê Jacyntho <kauekaj@gmail.com>
2022-06-08 21:00:13 -0300

    add IAmRich

---
+++ IAmRich/IAmRich/IAmRich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IAmRich
+//
+//  Created by kauekaj on 4/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
